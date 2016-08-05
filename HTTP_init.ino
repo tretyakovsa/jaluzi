f6e0cafe7d902457842665b8596ef1663c98c887
renat2985 <wul@inbox.lv>
2016-08-06 02:20:31 +0300

    Множество мелких изменений

--- HTTP_init.ino
+++ HTTP_init.ino
@@ -1,254 +1,255 @@
-//------------------------------------------
 // Меняет флаг для запуска сервопривода
 void MotorActiv() {
-  chaing = 1;
-  HTTP.send(200, "text/plain", "OK");
+ chaing = 1;
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 // Сохраняет все настройки в файле
 void handle_saveConfig() {
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 // Время вращения сервопривода
 void handle_TimeServo() {
-  TimeServo = HTTP.arg("t").toInt();
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ TimeServo = HTTP.arg("t").toInt();
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 // Время вращения сервопривода
 void handle_TimeServo2() {
-  TimeServo2 = HTTP.arg("t").toInt();
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ TimeServo2 = HTTP.arg("t").toInt();
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 // Установка времянной зоны
 void handle_TimeZone() {
-  timezone = HTTP.arg("timezone").toInt();
-  Time_init(timezone);
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ timezone = HTTP.arg("timezone").toInt();
+ Time_init(timezone);
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 // Установка параметров сети
 void handle_Set_Ssid() {
-  _ssid = HTTP.arg("ssid");
-  _password = HTTP.arg("password");
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ _ssid = HTTP.arg("ssid");
+ _password = HTTP.arg("password");
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 // Установка параметров сети
 void handle_Set_Ssdp() {
-  SSDP_Name = HTTP.arg("ssdp");
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ SSDP_Name = HTTP.arg("ssdp");
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
-// Установка параметров точки доступа
+
+//Установка параметров точки доступа
 void handle_Set_Ssidap() {
-  _ssidAP = HTTP.arg("ssidAP");
-  _passwordAP = HTTP.arg("passwordAP");
-  if (HTTP.arg("onOffAP") == "true") {
+ _ssidAP = HTTP.arg("ssidAP");
+ _passwordAP = HTTP.arg("passwordAP");
+ if (HTTP.arg("onOffAP") == "true") {
   _setAP = "1";
-}
-else {
+ } else {
   _setAP = "0";
+ }
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-saveConfig();
-HTTP.send(200, "text/plain", "OK");
-}
-//------------------------------------------
-// Время из сети
+
+//Время из сети
 void handle_Time() {
-  Time_init(timezone);
-  String Time = XmlTime();
-  HTTP.send(200, "text/plain", "Время синхронизовано: " + Time);
+ Time_init(timezone);
+ String Time = XmlTime();
+ HTTP.send(200, "text/plain", "Время синхронизовано: " + Time);
 }
-// Установка скорости вращения
+
+//Установка скорости вращения
 void handle_speed() {
-  speed = HTTP.arg("speed").toInt();
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ speed = HTTP.arg("speed").toInt();
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 //Задать время открытия
 void handle_Time_Up() {
-  TimeUp = HTTP.arg("open");
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ TimeUp = HTTP.arg("open");
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//------------------------------------------
+
 //Задать время закрытия
 void handle_Time_Down() {
-  TimeDown = HTTP.arg("close");
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+ TimeDown = HTTP.arg("close");
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
-//----------------------------------
+
 //Задать время закрытия
-void handlekolibr() {
-  kolibr = HTTP.arg("kolibr").toInt();
-  saveConfig();
-  HTTP.send(200, "text/plain", "OK");
+void handle_Kolibr() {
+ kolibr = HTTP.arg("kolibr").toInt();
+ saveConfig();
+ HTTP.send(200, "text/plain", "OK");
 }
 
 void HTTP_init(void) {
-  // SSDP дескриптор
-  HTTP.on("/description.xml", HTTP_GET, []() {
-    SSDP.schema(HTTP.client());
-  });
-  // Добавляем функцию Update для перезаписи прошивки по WiFi при 1М(256K SPIFFS) и выше
-  update();
+ // SSDP дескриптор
+ HTTP.on("/description.xml", HTTP_GET, []() {
+  SSDP.schema(HTTP.client());
+ });
+ // Добавляем функцию Update для перезаписи прошивки по WiFi при 1М(256K SPIFFS) и выше
+ update();
 
-  HTTP.on("/Motor", MotorActiv);            // запуск мотора напровление храниться в переменной
-  HTTP.on("/Timeservo", handle_TimeServo);  // установка времени вращения сервопривода
-  HTTP.on("/Timeservo2", handle_TimeServo2);  // установка времени вращения сервопривода
-  HTTP.on("/TimeZone", handle_TimeZone);    // Установка времянной зоны
-  HTTP.on("/Time", handle_Time);            // Синхронизировать время из сети
-  HTTP.on("/TimeUp", handle_Time_Up);       // Установить время открытия
-  HTTP.on("/TimeDown", handle_Time_Down);   // Установить время закрытия
-  HTTP.on("/ssdp", handle_Set_Ssdp);        // Установить имя устройства
-  HTTP.on("/ssid", handle_Set_Ssid);        // Установить имя и пароль роутера
-  HTTP.on("/ssidap", handle_Set_Ssidap);    // Установить имя и пароль для точки доступа
-  HTTP.on("/speed", handle_speed);          // Установить скорость вращения сервопривода
-  HTTP.on("/Save", handle_saveConfig);      // Сохранить настройки в файл
-  HTTP.on("/config.xml", handleConfigXML);   // формирование config_xml страницы для передачи данных в web интерфейс
-  HTTP.on("/iplocation.xml", handleIplocationXML);   // формирование iplocation_xml страницы для передачи данных в web интерфейс
-  HTTP.on("/kolibr", handlekolibr);         // колибруем серву
-  HTTP.on("/block", block);                 // Блок для device.htm
-  // Запускаем HTTP сервер
-  // HTTP.sendHeader("Cache-Control","max-age=2592000, must-revalidate");
-  HTTP.on("/Devices", inquirySSDP);                 // Блок для
-  // Запускаем HTTP сервер
-  HTTP.begin();
+ HTTP.on("/motor", MotorActiv);            // запуск мотора напровление храниться в переменной
+ HTTP.on("/Timeservo", handle_TimeServo);  // установка времени вращения сервопривода
+ HTTP.on("/Timeservo2", handle_TimeServo2);// установка времени вращения сервопривода
+ HTTP.on("/TimeZone", handle_TimeZone);    // Установка времянной зоны
+ HTTP.on("/Time", handle_Time);            // Синхронизировать время из сети
+ HTTP.on("/TimeUp", handle_Time_Up);       // Установить время открытия
+ HTTP.on("/TimeDown", handle_Time_Down);   // Установить время закрытия
+ HTTP.on("/ssdp", handle_Set_Ssdp);        // Установить имя устройства
+ HTTP.on("/ssid", handle_Set_Ssid);        // Установить имя и пароль роутера
+ HTTP.on("/ssidap", handle_Set_Ssidap);    // Установить имя и пароль для точки доступа
+ HTTP.on("/speed", handle_speed);          // Установить скорость вращения сервопривода
+ HTTP.on("/Save", handle_saveConfig);      // Сохранить настройки в файл
+ HTTP.on("/config.xml", handle_ConfigXML);  // формирование config_xml страницы для передачи данных в web интерфейс
+ HTTP.on("/iplocation.xml", handle_IplocationXML);   // формирование iplocation_xml страницы для передачи данных в web интерфейс
+ HTTP.on("/kolibr", handle_Kolibr);         // колибруем серву
+ HTTP.on("/block", handle_Block);                 // Блок для device.htm
+ // Запускаем HTTP сервер
+ // HTTP.sendHeader("Cache-Control","max-age=2592000, must-revalidate");
+ HTTP.on("/devices", inquirySSDP);         // Блок для
+ // Запускаем HTTP сервер
+ HTTP.begin();
 }
-
 
 // Получение текущего времени
 String XmlTime(void) {
-  String Time = ""; // Строка для результатов времени
-  int i = 0; // Здесь будем хранить позицию первого символа :
-  time_t now = time(nullptr); // получаем времяс помощью библиотеки time.h
-  Time += ctime(&now); // Преобразуем время в строку формата
-  i = Time.indexOf(":"); //Ишем позицию первого символа :
-  Time = Time.substring(i - 2, i + 6); // Выделяем из строки 2 символа перед символом : и 6 символов после
-  return Time; // Возврашаем полученное время
+ String Time = ""; // Строка для результатов времени
+ int i = 0; // Здесь будем хранить позицию первого символа :
+ time_t now = time(nullptr); // получаем времяс помощью библиотеки time.h
+ Time += ctime(&now); // Преобразуем время в строку формата
+ i = Time.indexOf(":"); //Ишем позицию первого символа :
+ Time = Time.substring(i - 2, i + 6); // Выделяем из строки 2 символа перед символом : и 6 символов после
+ return Time; // Возврашаем полученное время
 }
 
-void handleConfigXML() {
-  XML = "<?xml version='1.0'?>";
-  XML += "<Donnees>";
-  // Имя SSDP
-  XML += "<SSDP>";
-  XML += SSDP_Name;
-  XML += "</SSDP>";
-  // Статус AP
-  XML += "<onOffAP>";
-  XML += _setAP;
-  XML += "</onOffAP>";
-  // Имя сети
-  XML += "<ssid>";
-  XML += _ssid;
-  XML += "</ssid>";
-  // Пароль сети
-  XML += "<password>";
-  if (_password == NULL) {
-    XML += " ";
-  } else {
-    XML += _password;
-  }
-  XML += "</password>";
-  // Имя точки доступа
-  XML += "<ssidAP>";
-  XML += _ssidAP;
-  XML += "</ssidAP>";
-  // Пароль точки доступа
-  XML += "<passwordAP>";
-  if (_passwordAP == NULL) {
-    XML += " ";
-  } else {
-    XML += _passwordAP;
-  }
-  XML += "</passwordAP>";
-  // Времянная зона
-  XML += "<timezone>";
-  XML += timezone;
-  XML += "</timezone>";
-  // Скорость вращения
-  XML += "<speed>";
-  XML += speed;
-  XML += "</speed>";
-  //  Время врашения
-  XML += "<timeservo>";
-  XML += TimeServo;
-  XML += "</timeservo>";
-  //  Время врашения
-  XML += "<timeservo2>";
-  XML += TimeServo2;
-  XML += "</timeservo2>";
-  // Время открытия
-  XML += "<TimeUp>";
-  XML += TimeUp;
-  XML += "</TimeUp>";
-  // Время закрытия
-  XML += "<TimeDown>";
-  XML += TimeDown;
-  XML += "</TimeDown>";
-  // Текущее время
-  XML += "<time>";
-  XML += XmlTime();
-  XML += "</time>";
-  // Колибруе серву
-  XML += "<kolibr>";
-  XML += kolibr;
-  XML += "</kolibr>";
-  // Статус жалюзи
-  XML += "<state>";
-  XML += state0;
-  XML += "</state>";
-  XML += "</Donnees>";
-  HTTP.send(200, "text/xml", XML);
+void handle_ConfigXML() {
+ XML = "<?xml version='1.0'?>";
+ XML += "<Donnees>";
+ // Имя SSDP
+ XML += "<SSDP>";
+ XML += SSDP_Name;
+ XML += "</SSDP>";
+ // Статус AP
+ XML += "<onOffAP>";
+ XML += _setAP;
+ XML += "</onOffAP>";
+ // Имя сети
+ XML += "<ssid>";
+ XML += _ssid;
+ XML += "</ssid>";
+ // Пароль сети
+ XML += "<password>";
+ if (_password == NULL) {
+  XML += " ";
+ } else {
+  XML += _password;
+ }
+ XML += "</password>";
+ // Имя точки доступа
+ XML += "<ssidAP>";
+ XML += _ssidAP;
+ XML += "</ssidAP>";
+ // Пароль точки доступа
+ XML += "<passwordAP>";
+ if (_passwordAP == NULL) {
+  XML += " ";
+ } else {
+  XML += _passwordAP;
+ }
+ XML += "</passwordAP>";
+ // Времянная зона
+ XML += "<timezone>";
+ XML += timezone;
+ XML += "</timezone>";
+ // Скорость вращения
+ XML += "<speed>";
+ XML += speed;
+ XML += "</speed>";
+ //  Время врашения
+ XML += "<timeservo>";
+ XML += TimeServo;
+ XML += "</timeservo>";
+ //  Время врашения
+ XML += "<timeservo2>";
+ XML += TimeServo2;
+ XML += "</timeservo2>";
+ // Время открытия
+ XML += "<TimeUp>";
+ XML += TimeUp;
+ XML += "</TimeUp>";
+ // Время закрытия
+ XML += "<TimeDown>";
+ XML += TimeDown;
+ XML += "</TimeDown>";
+ // Текущее время
+ XML += "<time>";
+ XML += XmlTime();
+ XML += "</time>";
+ // Колибруе серву
+ XML += "<kolibr>";
+ XML += kolibr;
+ XML += "</kolibr>";
+ // Статус жалюзи
+ XML += "<state>";
+ XML += state0;
+ XML += "</state>";
+ XML += "</Donnees>";
+ HTTP.send(200, "text/xml", XML);
 }
 
-void block() {
-
-  XML = "<div class=\"block col-md-5\">";
-  XML += "<h5>";
-  XML += SSDP_Name;
-  XML += "</h5>";
-  XML += "<div class=\"alert alert-dismissible alert-warning\"><b>Обратите внимание</b>";
-  XML += "<br>Время закрытия: ";
-  XML += TimeUp;
-  XML += "<br>Время открытия: ";
-  XML += TimeDown;
-  XML += "</div>";
-  XML += "<input class=\"btn btn-block btn-lg btn-primary\" value=\"Открыть/закрыть жалюзи\" onclick=\"ajax('";
-  XML += WiFi.localIP().toString();
-  XML += "/Motor', this);\" type=\"submit\">";
-  XML += "<hr>";
-  XML += "<div class=\"alert alert-dismissible alert-info\">Изменить конфигурацию устройсва вы можете на странице управления</div>";
-  XML += "<a class=\"btn btn-block btn-default\" href=\"http://";
-  XML += WiFi.localIP().toString();
-  XML += "\">Страница управления</a>";
-  XML += "</div>";
-  HTTP.send(200, "text/plain", XML);
+void handle_Block() {
+ XML = "<div class=\"block col-md-5\">";
+ XML += "<h5>";
+ XML += SSDP_Name;
+ XML += "</h5>";
+ XML += "<div class=\"alert alert-dismissible alert-warning\"><b>Обратите внимание</b>";
+ XML += "<br>Время закрытия: ";
+ XML += TimeUp;
+ XML += "<br>Время открытия: ";
+ XML += TimeDown;
+ XML += "</div>";
+ XML += "<input class=\"btn btn-block btn-lg btn-primary\" value=\"";
+ if (state0 == '1') {
+  XML += "Закрыть";
+ } else {
+  XML += "Открыть";
+ }
+ XML += " жалюзи\" onclick=\"ajax('";
+ XML += WiFi.localIP().toString();
+ XML += "/motor', this);\" type=\"submit\">";
+ XML += "<hr>";
+ XML += "<div class=\"alert alert-dismissible alert-info\">Изменить конфигурацию устройсва вы можете на странице управления</div>";
+ XML += "<a class=\"btn btn-block btn-default\" href=\"http://";
+ XML += WiFi.localIP().toString();
+ XML += "\">Страница управления</a>";
+ XML += "</div>";
+ HTTP.send(200, "text/plain", XML);
 }
 
-void handleIplocationXML() {
-  IplocationXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
-  IplocationXML += "<List>";
-  IplocationXML += "<location>";
-  IplocationXML += "<ip>";
-  IplocationXML += WiFi.localIP().toString();
-  IplocationXML += "</ip>";
-  IplocationXML += Devices;
-  IplocationXML += "</location>";
-  IplocationXML += "</List>";
-  HTTP.send(200, "text/xml", IplocationXML);
+void handle_IplocationXML() {
+ XML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>";
+ XML += "<List>";
+ XML += "<location>";
+ XML += "<ip>";
+ XML += WiFi.localIP().toString();
+ XML += "</ip>";
+ XML += Devices;
+ XML += "</location>";
+ XML += "</List>";
+ HTTP.send(200, "text/xml", XML);
 }
-
-
--- jaluzi.ino
+++ jaluzi.ino
@@ -8,7 +8,6 @@
 #include <WiFiUdp.h>            //Содержится в пакете
 #include <ArduinoJson.h>
 
-
 // Web интерфейс для устройства
 ESP8266WebServer HTTP(80);
 // Для файловой системы
@@ -37,7 +36,6 @@ String SSDP_Name = "jalousie";      // SSDP
 String TimeUp = "08:00:00";      // время открытия
 String TimeDown = "21:00:00";    // время закрытия
 String Devices = "";    // IP адреса устройств в сети
-String IplocationXML="";
 int timezone = 3;        // часовой пояс GTM
 int Led1 = 12;           // индикатор движения вверх
 int Led2 = 13;           // индикатор движения вниз
@@ -57,78 +55,74 @@ const char* ssdpAdress = "192.168.1.255";
 // A UDP instance to let us send and receive packets over UDP
 WiFiUDP udp;
 
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(Tach0, INPUT);
-  pinMode(Led1, OUTPUT);
-  pinMode(Led2, OUTPUT);
-  Serial.println("");
-  // Параметры памяти ESP справочно можно закаментировать
-  CheckFlashConfig();
-  // Включаем работу с файловой системой
-  FS_init();
-  // Загружаем настройки из файла
-  loadConfig();
-  // Подключаем сервомотор
-  myservo.attach(servo_pin);
-  //myservo.write(kolibr);
-  // Кнопка будет работать по прерыванию
-  attachInterrupt(Tach0, Tach_0, FALLING);
-  //Запускаем WIFI
-  WIFIAP_Client();
-  // Закускаем UDP
-  udp.begin(localPort);
-  //udp.beginMulticast(WiFi.localIP(), ssdpAdress1, ssdpPort);
-  Serial.print("Local port: ");
-  Serial.println(udp.localPort());
-  //настраиваем HTTP интерфейс
-  HTTP_init();
-  Serial.println("HTTP Ready!");
-  //запускаем SSDP сервис
-  SSDP_init();
-  Serial.println("SSDP Ready!");
-  // Включаем время из сети
+void setup() {
+ Serial.begin(115200);
+ pinMode(Tach0, INPUT);
+ pinMode(Led1, OUTPUT);
+ pinMode(Led2, OUTPUT);
+ Serial.println("");
+ // Параметры памяти ESP справочно можно закаментировать
+ CheckFlashConfig();
+ // Включаем работу с файловой системой
+ FS_init();
+ // Загружаем настройки из файла
+ loadConfig();
+ // Подключаем сервомотор
+ myservo.attach(servo_pin);
+ //myservo.write(kolibr);
+ // Кнопка будет работать по прерыванию
+ attachInterrupt(Tach0, Tach_0, FALLING);
+ //Запускаем WIFI
+ WIFIAP_Client();
+ // Закускаем UDP
+ udp.begin(localPort);
+ //udp.beginMulticast(WiFi.localIP(), ssdpAdress1, ssdpPort);
+ Serial.print("Local port: ");
+ Serial.println(udp.localPort());
+ //настраиваем HTTP интерфейс
+ HTTP_init();
+ Serial.println("HTTP Ready!");
+ //запускаем SSDP сервис
+ SSDP_init();
+ Serial.println("SSDP Ready!");
+ // Включаем время из сети
+ Time_init(timezone);
+ // Будет выполняться каждую секунду проверяя будильники
+ tickerAlert.attach(1, alert);
+}
+
+void loop() {
+ HTTP.handleClient();
+ delay(1);
+ handleUDP();
+ if (chaing && !chaing1) {
+  noInterrupts();
+  switch (state0) {
+   case 0:
+    MotorUp();
+    break;
+   case 1:
+    MotorDown();
+    break;
+  }
+  interrupts();
+ }
+ if (chaingtime) {
   Time_init(timezone);
-  // Будет выполняться каждую секунду проверяя будильники
-  tickerAlert.attach(1, alert);
+  chaingtime=0;
+ }
 }
 
-void loop()
-{
-  HTTP.handleClient();
-  delay(1);
-  handleUDP();
-  if (chaing && !chaing1) {
-    noInterrupts();
-    switch (state0) {
-
-      case 0:
-        MotorUp();
-        break;
-      case 1:
-        MotorDown();
-        break;
-    }
-    interrupts();
-  }
-  if (chaingtime) {
-    Time_init(timezone);
-    chaingtime=0;
-  }
-}
 // Вызывается каждую секунду в обход основного циклу.
 void alert() {
-  String Time=XmlTime();
-  if (TimeUp.compareTo(Time) == 0) {
-    MotorUp();
-  }
-  if (TimeDown.compareTo(Time) == 0) {
-    MotorDown();
-  }
-  if (kolibrTime.compareTo(Time) == 0) {
-    chaingtime=1;
-  }
-
+ String Time=XmlTime();
+ if (TimeUp.compareTo(Time) == 0) {
+  MotorUp();
+ }
+ if (TimeDown.compareTo(Time) == 0) {
+  MotorDown();
+ }
+ if (kolibrTime.compareTo(Time) == 0) {
+  chaingtime=1;
+ }
 }
-
