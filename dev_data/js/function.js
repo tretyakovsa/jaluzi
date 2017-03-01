2c64141e6bbc068a0c56acc49d2665bfa12e888d
renat2985 <wul@inbox.lv>
2017-03-01 15:31:22 +0200

    id fix

--- dev_data/js/function.js
+++ dev_data/js/function.js
@@ -99,7 +99,7 @@ function toggle(target,status) {
  }
 }
 
-function language(submit){
+function setLang(submit){
  var xmlHttp=createXmlHttpObject();
  xmlHttp.open('GET',"/lang?set="+submit,true);
  xmlHttp.send(null);
@@ -116,7 +116,7 @@ function LoadWifi(ssids){
   var jsonWifi=JSON.parse(xmlHttp.responseText);
   var html = "";
   for(i = 0;i<jsonWifi.networks.length;i++) {
-   html += '<li><a href="#" onclick="val(\'ssid\',\''+jsonWifi.networks[i].ssid+'\');toggle(\'ssid-select\');document.getElementById(\'ssid-name\').innerHTML=\''+jsonWifi.networks[i].ssid+'\';return false"><b>' +jsonWifi.networks[i].ssid + jsonWifi.networks[i].pass + '</b> (' +jsonWifi.networks[i].dbm + ' dBm)</a></li>';
+   html += '<li><a href="#" onclick="val(\'ssid\',\''+jsonWifi.networks[i].ssid+'\');toggle(\'ssid-select\');document.getElementById(\'ssid-name\').innerHTML=\''+jsonWifi.networks[i].ssid+'\';return false"><div style="float:right">'+(jsonWifi.networks[i].pass?'&#x1f512;':'')+' <span class="label label-info">'+(jsonWifi.networks[i].dbm+100)+'%</span> <span class="label label-default">'+jsonWifi.networks[i].dbm+' dBm</span></div><b>'+jsonWifi.networks[i].ssid+'</b></a></li>';
   }
   document.getElementById(ssids).innerHTML = html+'<li><a href="#" onclick="toggle(\'ssid-group\');toggle(\'ssid\');return false"><b>'+jsonResponse.LangHiddenWifi+'</b></a></li>';
  }
@@ -131,9 +131,26 @@ function LoadLang(langids){
   var html = '';
   for(var key in jsonLang) {
    var view_lang = jsonLang[key].name.substr(10,2);
-   html += '<li><a href="#" onclick="language(\''+view_lang+'\')" title="'+jsonLang[key].name+'">'+view_lang+'</a></li>';
+   html += '<li><a href="#" onclick="setLang(\''+view_lang+'\')" title="'+jsonLang[key].name+'">'+view_lang+'</a></li>';
   }
   document.getElementById(langids).innerHTML = html;
+ }
+}
+
+function LoadTimer(timerids){
+ var xhttp=createXmlHttpObject();
+ xhttp.open("GET", "/timer.save.json", true);
+ xhttp.send(null);
+ xhttp.onload = function(e) {
+  var timers=JSON.parse(xhttp.responseText);
+  var html = '';
+  for (var i = 0; i < timers.timer.length; i++) {
+   if (timers.timer[i].trigger == "on") {timers.timer[i].trigger = '<span class="label label-success">'+jsonResponse["LangOn."]+'</span>';}
+   if (timers.timer[i].trigger == "off") {timers.timer[i].trigger = '<span class="label label-danger">'+jsonResponse["LangOff."]+'</span>';}
+   timers.timer[i].day = jsonResponse["Lang"+timers.timer[i].day];
+   html += '<li>'+timers.timer[i].trigger+' <b>'+timers.timer[i].day+'<\/b> '+timers.timer[i].time+'<\/li>';
+  }
+  document.getElementById(timerids).innerHTML = html;
  }
 }
 
